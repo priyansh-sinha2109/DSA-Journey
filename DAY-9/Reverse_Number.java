221663d224b907626cfe27661730fb4058de37c6
priyansh-sinha2109 <priyanshsinha2109@gmail.com>
2026-07-21 23:06:49 +0530

    Reverse a Number -> 20 July Problem not solve

--- DAY-9/Reverse_Number.java
+++ DAY-9/Reverse_Number.java
@@ -1,5 +1,23 @@
-package DAY-9;
+import java.util.Scanner;
 
 public class Reverse_Number {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int reverse = Reverse(n);
+        System.out.print("After Reverse : " + reverse);
+    }
+
+    public static int Reverse(int x) {
+        int ans = 0;
+        while(x != 0){
+            int lastDigit = x % 10;
+            if(ans > Integer.MAX_VALUE / 10 || ans < Integer.MIN_VALUE / 10){
+                return 0;
+            }
+            ans = (ans * 10) + lastDigit; 
+            x = x / 10;
+        }
+        return ans;
+    }
 }
