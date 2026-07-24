5e82360ac4848245eecc964f9a479e9f4501be00
priyansh-sinha2109 <priyanshsinha2109@gmail.com>
2026-07-24 18:16:26 +0530

    Factorial question solve

--- DAY-12/Factorial.java
+++ DAY-12/Factorial.java
@@ -1,5 +1,17 @@
-package DAY-12;
+import java.util.Scanner;
 
 public class Factorial {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input .nextInt();
+        System.out.println(factorial(n));
+    }
+
+    // 
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        } 
+        return n * factorial(n - 1);
+    }
 }
